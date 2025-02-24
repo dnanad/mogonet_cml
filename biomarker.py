a1998a224c933f9f8f98c345ac73dad8d94d161b
dnanad <ananddeshpande13@gmail.com>
2025-02-24 23:38:34 +0100

    parse args added

--- biomarker.py
+++ biomarker.py
@@ -3,17 +3,30 @@
 
 import os
 import copy
+import argparse
+import json
 from feat_importance import cal_feat_imp, summarize_imp_feat
 from utils import find_numFolders_maxNumFolders
 
-if __name__ == "__main__":
-    # "discovery_cohort"  # "PE_cfRNA"  # "disc_coho3"  # "PE_cfRNA_pre"  # "TEST_DATA"  # "PE_cfRNA"  # "PE_cfRNA_pre"  # "TEST_DATA_copy"  # "ROSMAP" #"BRCA
-    data_folder = "cfrna_metabo_disc225(0.001_95%)_TriStrat2"  # "cfrna_disc225(0.001_95%)"  # "metabo_disc225"  # "trial"  # discovery_cohort"  # "PE_cfRNA"  # "discovery_cohort"  # "PB2_cfRNA"  # "PE_cfRNA_pre"  # "TEST_DATA"  # "PE_cfRNA"  # "TEST_DATA"  # "PE_cfRNA"  # "TEST_DATA"  # "ROSMAP"
-    stratify = True
-    CV = True
-    n_splits = 5
-    num_epoch_pretrain = 400
-    num_epoch = 1200
+
+def main(
+    data_folder: str,
+    stratify: bool,
+    CV: bool,
+    n_splits: int,
+    num_epoch_pretrain: int,
+    num_epoch: int,
+    num_class: int,
+    adj_parameter: int,
+    dim_he_list: list,
+) -> None:
+    print("Calculating feature importance")
+    print(f"Data folder: {data_folder}")
+    print(f"Stratify: {stratify}")
+    print(f"CV: {CV}")
+    print(f"Number of splits: {n_splits}")
+    print(f"Number of pretrain epochs: {num_epoch_pretrain}")
+    print(f"Number of epochs: {num_epoch}")
 
     rootpath = os.path.dirname(os.path.realpath(__file__))
     data_folder_path = os.path.join(rootpath, "data", data_folder)
@@ -40,86 +53,14 @@ if __name__ == "__main__":
 
     exp_epoch = str(num_epoch_pretrain) + "_" + str(num_epoch)
     exp = os.path.join(model_folder_path, exp_epoch)
-
-    dt_string = (
-        "20240408-013504"
-        # "20240123-160739"  # cpm95_cfrna001 strat_CV 400_1200
-        # "20240122-180033"  # cpm95_cfrna001_metabo2 strat_CV 400_1200
-        # "20240119-105801"  # metabo strat_CV 400_1200
-        # "20240118-195725"  # cfRNA start_CV 400_1200
-        # "20240118-193318"  # LATEST strat_CV 400_1200
-        # "20240117-140446"  # strat_CV 400_1200
-        # "20240117-133600"  # no_strat CV
-        # "20240112-190638"
-        # "20230914-223605"  # PE_cfRNA_pre CV
-        # "20230907-182642"  # disc_coho3 CV
-        # "20230907-182151"  # disc_coho1 CV
-        # "20230907-181823"  # disc_coho1 NO_CV
-        # "20230907-004649"  # disc coho CV
-        # "20230906-153927"  # discovery cohort CV
-        # "20230906-152343" discovery chohort NO_CV
-        # "20230817-143951"
-        # "20230818-161111"
-        # "20230817-145402"
-        # "20230817-150345"  # "20230817-143951"  # 20230817-122038"  # "20230816-154623"
-    )
+    print(f"Model folder: {exp}")
+    print(f"Model: {os.listdir(exp)[0]}")
+    dt_string = os.listdir(exp)[0]
     latest_model = os.path.join(exp, dt_string)
 
     view_list, _ = find_numFolders_maxNumFolders(data_folder_path)
-    # view_list = [1]
     num_view = len(view_list)
     topn = 40
-
-    if (
-        data_folder == "cfrna_metabo_disc225(0.001_95%)_TriStrat2"
-    ):  # "cfrna_metabo_disc225":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [200, 200, 100]
-    if data_folder == "metabo_disc225":  # "cfrna_metabo_disc225":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [200, 200, 100]
-    if data_folder == "disc_coho1":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "disc_coho3":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "disc_coho":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "discovery_cohort":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "PE_cfRNA_pre":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "PE_cfRNA":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [400, 400, 200]
-    if data_folder == "TEST_DATA_copy":
-        num_class = 2  # number of classes
-        adj_parameter = 2  # number of neighbors for each node
-        dim_he_list = [400, 400, 200]  # hidden dimensions for each view
-    if data_folder == "TEST_DATA":
-        num_class = 2  # number of classes
-        adj_parameter = 2  # number of neighbors for each node
-        dim_he_list = [400, 400, 200]  # hidden dimensions for each view
-    if data_folder == "ROSMAP":
-        num_class = 2
-        adj_parameter = 2
-        dim_he_list = [200, 200, 100]
-    if data_folder == "BRCA":
-        num_class = 5
-        adj_parameter = 10
-        dim_he_list = [400, 400, 200]
 
     featimp_list_list = []
     if CV:
@@ -138,4 +79,62 @@ if __name__ == "__main__":
 
     summarize_imp_feat(
         featimp_list_list, exp, topn=topn, feat_file_suffix=feat_file_suffix
+    )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Biomarker identification")
+    parser.add_argument("--data_folder", required=True, help="Path to the data folder")
+    parser.add_argument("--stratify", action="store_true", help="Stratify!")
+    parser.add_argument(
+        "--no_stratify", action="store_false", dest="stratify", help="Do not stratify"
+    )
+    parser.add_argument(
+        "--CV", action="store_true", help="Whether to use cross-validation"
+    )
+    parser.add_argument(
+        "--no_CV", action="store_false", dest="CV", help="Do not use cross-validation"
+    )
+    parser.add_argument(
+        "--n_splits",
+        type=int,
+        required=True,
+        help="Number of splits for cross-validation",
+    )
+    parser.add_argument(
+        "--num_epoch_pretrain",
+        type=int,
+        required=True,
+        help="Number of pretrain epochs",
+    )
+    parser.add_argument("--num_epoch", type=int, required=True, help="Number of epochs")
+
+    parser.add_argument(
+        "--num_class", type=int, required=True, help="Number of classes"
+    )
+    parser.add_argument(
+        "--adj_parameter", type=int, required=True, help="Adjacency parameter"
+    )
+    parser.add_argument(
+        "--dim_he_list",
+        type=json.loads,
+        required=True,
+        help="Dimension list for hidden layers",
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(
+        data_folder=args.data_folder,
+        stratify=args.stratify,
+        CV=args.CV,
+        n_splits=args.n_splits,
+        num_epoch_pretrain=args.num_epoch_pretrain,
+        num_epoch=args.num_epoch,
+        num_class=args.num_class,
+        adj_parameter=args.adj_parameter,
+        dim_he_list=args.dim_he_list,
     )
